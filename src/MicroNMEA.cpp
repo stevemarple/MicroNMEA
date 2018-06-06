626d7c58ac82f659455d75d47a4dc0a9060b5937
Steve Marple <stevemarple@googlemail.com>
2018-06-06 22:05:04 +0100

    Fix indents

--- src/MicroNMEA.cpp
+++ src/MicroNMEA.cpp
@@ -10,146 +10,146 @@ using namespace std;
 
 static long exp10(uint8_t b)
 {
-  long r = 1;
-  while (b--)
-    r *= 10;
-  return r;
+	long r = 1;
+	while (b--)
+		r *= 10;
+	return r;
 }
 
 
 static char toHex(uint8_t nibble)
 {
-  if (nibble >= 10)
-    return nibble + 'A' - 10;
-  else
-    return nibble + '0';
+	if (nibble >= 10)
+		return nibble + 'A' - 10;
+	else
+		return nibble + '0';
 
 }
 
 
 const char* MicroNMEA::skipField(const char* s)
 {
-  if (s == NULL)
-    return NULL;
+	if (s == NULL)
+		return NULL;
 
-  while (!isEndOfFields(*s)) {
-    if (*s == ',') {
-      // Check next character
-      if (isEndOfFields(*++s))
-	break;
-      else
-	return s;
-    }
-    ++s;
-  }
-  return NULL; // End of string or valid sentence
+	while (!isEndOfFields(*s)) {
+		if (*s == ',') {
+			// Check next character
+			if (isEndOfFields(*++s))
+				break;
+			else
+				return s;
+		}
+		++s;
+	}
+	return NULL; // End of string or valid sentence
 }
 
 
 unsigned int MicroNMEA::parseUnsignedInt(const char *s, uint8_t len)
 {
-  int r = 0;
-  while (len--)
-    r = 10 * r + *s++ - '0';
-  return r;
+	int r = 0;
+	while (len--)
+		r = 10 * r + *s++ - '0';
+	return r;
 }
 
 
 long MicroNMEA::parseFloat(const char* s, uint8_t log10Multiplier, const char** eptr)
 {
-  int8_t neg = 1;
-  long r = 0;
-  while (isspace(*s))
-    ++s;
-  if (*s == '-') {
-    neg = -1;
-    ++s;
-  }
-  else if (*s == '+')
-    ++s;
-  
-  while (isdigit(*s))
-    r = 10*r + *s++ - '0';
-  r *= exp10(log10Multiplier);
+	int8_t neg = 1;
+	long r = 0;
+	while (isspace(*s))
+		++s;
+	if (*s == '-') {
+		neg = -1;
+		++s;
+	}
+	else if (*s == '+')
+		++s;
 
-  if (*s == '.') {
-    ++s;
-    long frac = 0;
-    while (isdigit(*s) && log10Multiplier) {
-      frac = 10 * frac + *s++ -'0';
-      --log10Multiplier;
-    }
-    frac *= exp10(log10Multiplier);
-    r += frac;
-  }
-  r *= neg; // Include effect of any minus sign
+	while (isdigit(*s))
+		r = 10*r + *s++ - '0';
+	r *= exp10(log10Multiplier);
 
-  if (eptr)
-    *eptr = skipField(s);
+	if (*s == '.') {
+		++s;
+		long frac = 0;
+		while (isdigit(*s) && log10Multiplier) {
+			frac = 10 * frac + *s++ -'0';
+			--log10Multiplier;
+		}
+		frac *= exp10(log10Multiplier);
+		r += frac;
+	}
+	r *= neg; // Include effect of any minus sign
 
-  return r; 
+	if (eptr)
+		*eptr = skipField(s);
+
+	return r;
 }
 
 
 long MicroNMEA::parseDegreeMinute(const char* s, uint8_t degWidth,
-			     const char **eptr)
+								  const char **eptr)
 {
-  if (*s == ',') {
-    if (eptr)
-      *eptr = skipField(s);
-    return 0;
-  }
-  long r = parseUnsignedInt(s, degWidth) * 1000000L;
-  s += degWidth;
-  r += parseFloat(s, 6, eptr) / 60;
-  return r;
+	if (*s == ',') {
+		if (eptr)
+			*eptr = skipField(s);
+		return 0;
+	}
+	long r = parseUnsignedInt(s, degWidth) * 1000000L;
+	s += degWidth;
+	r += parseFloat(s, 6, eptr) / 60;
+	return r;
 }
 
 
 const char* MicroNMEA::parseField(const char* s, char *result, int len)
 {
-  if (s == NULL)
-    return NULL;
+	if (s == NULL)
+		return NULL;
 
-  int i = 0;
-  while (*s != ',' && !isEndOfFields(*s)) {
-    if (result && i++ < len)
-      *result++ = *s;
-    ++s;
-  }
-  if (result && i < len)
-    *result = '\0'; // Terminate unless too long
+	int i = 0;
+	while (*s != ',' && !isEndOfFields(*s)) {
+		if (result && i++ < len)
+			*result++ = *s;
+		++s;
+	}
+	if (result && i < len)
+		*result = '\0'; // Terminate unless too long
 
-  if (*s == ',')
-    return ++s; // Location of start of next field
-  else
-    return NULL; // End of string or valid sentence
+	if (*s == ',')
+		return ++s; // Location of start of next field
+	else
+		return NULL; // End of string or valid sentence
 }
 
 
 const char* MicroNMEA::generateChecksum(const char* s, char* checksum)
 {
-  uint8_t c = 0;
-  // Initial $ is omitted from checksum, if present ignore it.
-  if (*s == '$')
-    ++s;
-  
-  while (*s != '\0' && *s != '*')
-    c ^= *s++;
+	uint8_t c = 0;
+	// Initial $ is omitted from checksum, if present ignore it.
+	if (*s == '$')
+		++s;
 
-  if (checksum) {
-    checksum[0] = toHex(c / 16);
-    checksum[1] = toHex(c % 16);
-  }
-  return s;
+	while (*s != '\0' && *s != '*')
+		c ^= *s++;
+
+	if (checksum) {
+		checksum[0] = toHex(c / 16);
+		checksum[1] = toHex(c % 16);
+	}
+	return s;
 }
 
 
 bool MicroNMEA::testChecksum(const char* s)
 {
-  char checksum[2];
-  const char* p = generateChecksum(s, checksum);
-  return *p == '*' && p[1] == checksum[0] && p[2] == checksum[1]; 
+	char checksum[2];
+	const char* p = generateChecksum(s, checksum);
+	return *p == '*' && p[1] == checksum[0] && p[2] == checksum[1];
 }
 
 
@@ -159,194 +159,193 @@ bool MicroNMEA::testChecksum(const char* s)
 // debugging don't compile it.
 Stream& MicroNMEA::sendSentence(Stream& s, const char* sentence)
 {
-  char checksum[3];
-  generateChecksum(sentence, checksum);
-  checksum[2] = '\0';
-  s.print(sentence);
-  s.print('*');
-  s.print(checksum);
-  s.print("\r\n");
-  return s;
+	char checksum[3];
+	generateChecksum(sentence, checksum);
+	checksum[2] = '\0';
+	s.print(sentence);
+	s.print('*');
+	s.print(checksum);
+	s.print("\r\n");
+	return s;
 }
 #endif
 
 
 MicroNMEA::MicroNMEA(void) :
-  _badChecksumHandler(NULL),
-  _unknownSentenceHandler(NULL)
+	_badChecksumHandler(NULL),
+	_unknownSentenceHandler(NULL)
 {
-  setBuffer(NULL, 0);
-  clear();
+	setBuffer(NULL, 0);
+	clear();
 }
 
 
 MicroNMEA::MicroNMEA(void* buf, uint8_t len) :
-  _badChecksumHandler(NULL),
-  _unknownSentenceHandler(NULL)
+	_badChecksumHandler(NULL),
+	_unknownSentenceHandler(NULL)
 {
-  setBuffer(buf, len);
-  clear();
+	setBuffer(buf, len);
+	clear();
 }
 
 
 void MicroNMEA::setBuffer(void* buf, uint8_t len)
 {
-  _bufferLen = len;
-  _buffer = (char*)buf;
-   _ptr = _buffer;
-  if (_bufferLen) {
-    *_ptr = '\0';
-    _buffer[_bufferLen - 1] = '\0';
-  }
+	_bufferLen = len;
+	_buffer = (char*)buf;
+	_ptr = _buffer;
+	if (_bufferLen) {
+		*_ptr = '\0';
+		_buffer[_bufferLen - 1] = '\0';
+	}
 }
 
 
 void MicroNMEA::clear(void)
 {
-  _navSystem = '\0';
-  _numSat = 0;
-  _hdop = 255;
-  _isValid = false;
-  _latitude = 999000000L;
-  _longitude = 999000000L;
-  _altitude = _speed = _course = LONG_MIN;
-  _altitudeValid = false;
-  _year = _month = _day = 0;
-  _hour = _minute = _second = 99;
-  _hundredths = 0;
+	_navSystem = '\0';
+	_numSat = 0;
+	_hdop = 255;
+	_isValid = false;
+	_latitude = 999000000L;
+	_longitude = 999000000L;
+	_altitude = _speed = _course = LONG_MIN;
+	_altitudeValid = false;
+	_year = _month = _day = 0;
+	_hour = _minute = _second = 99;
+	_hundredths = 0;
 }
 
 
 bool MicroNMEA::process(char c)
 {
-  if (_buffer == NULL || _bufferLen == 0)
-    return false;
-  if (c == '\0' || c == '\n' || c == '\r') {
-    // Terminate buffer then reset pointer
-    *_ptr = '\0'; 
-    _ptr = _buffer;
-    
-    if (*_buffer == '$' && testChecksum(_buffer)) {
-      // Valid message
-      const char* data;
-      if (_buffer[1] == 'G') {
-	_talkerID = _buffer[2];
-	data = parseField(&_buffer[3], &_messageID[0], sizeof(_messageID)); 
-      }
-      else {
-	_talkerID = '\0';
-	data = parseField(&_buffer[1], &_messageID[0], sizeof(_messageID)); 
-      }
-      
-      if (strcmp(&_messageID[0], "GGA") == 0)
-	return processGGA(data);
-      else if (strcmp(&_messageID[0], "RMC") == 0)
-	return processRMC(data);
-      else if (_unknownSentenceHandler)
-	(*_unknownSentenceHandler)(*this);
+	if (_buffer == NULL || _bufferLen == 0)
+		return false;
+	if (c == '\0' || c == '\n' || c == '\r') {
+		// Terminate buffer then reset pointer
+		*_ptr = '\0';
+		_ptr = _buffer;
 
-      return true; // New valid MicroNMEA sentence
-    }
-    else {
-      if (_badChecksumHandler && *_buffer != '\0') // don't send empty buffers as bad checksums!
-	(*_badChecksumHandler)(*this);
-    }
-  }
-  else {
-    *_ptr = c;
-    if (_ptr < &_buffer[_bufferLen - 1])
-      ++_ptr;
-  }
-  
-  return false;
+		if (*_buffer == '$' && testChecksum(_buffer)) {
+			// Valid message
+			const char* data;
+			if (_buffer[1] == 'G') {
+				_talkerID = _buffer[2];
+				data = parseField(&_buffer[3], &_messageID[0], sizeof(_messageID));
+			}
+			else {
+				_talkerID = '\0';
+				data = parseField(&_buffer[1], &_messageID[0], sizeof(_messageID));
+			}
+
+			if (strcmp(&_messageID[0], "GGA") == 0)
+				return processGGA(data);
+			else if (strcmp(&_messageID[0], "RMC") == 0)
+				return processRMC(data);
+			else if (_unknownSentenceHandler)
+				(*_unknownSentenceHandler)(*this);
+
+			return true; // New valid MicroNMEA sentence
+		}
+		else {
+			if (_badChecksumHandler && *_buffer != '\0') // don't send empty buffers as bad checksums!
+				(*_badChecksumHandler)(*this);
+		}
+	}
+	else {
+		*_ptr = c;
+		if (_ptr < &_buffer[_bufferLen - 1])
+			++_ptr;
+	}
+
+	return false;
 }
 
 
 const char* MicroNMEA::parseTime(const char* s)
 {
-  _hour = parseUnsignedInt(s, 2);
-  _minute = parseUnsignedInt(s + 2, 2);
-  _second = parseUnsignedInt(s + 4, 2);
-  _hundredths = parseUnsignedInt(s + 7, 2);
-  return skipField(s + 9);   
+	_hour = parseUnsignedInt(s, 2);
+	_minute = parseUnsignedInt(s + 2, 2);
+	_second = parseUnsignedInt(s + 4, 2);
+	_hundredths = parseUnsignedInt(s + 7, 2);
+	return skipField(s + 9);
 }
 
 
 const char* MicroNMEA::parseDate(const char* s)
 {
-  _day = parseUnsignedInt(s, 2);
-  _month = parseUnsignedInt(s + 2, 2);
-  _year = parseUnsignedInt(s + 4, 2) + 2000;
-  return skipField(s + 6);
+	_day = parseUnsignedInt(s, 2);
+	_month = parseUnsignedInt(s + 2, 2);
+	_year = parseUnsignedInt(s + 4, 2) + 2000;
+	return skipField(s + 6);
 }
 
 
 bool MicroNMEA::processGGA(const char *s)
 {
-  // If GxGSV messages are received _talker_ID can be changed after
-  // other MicroNMEA sentences. Compatibility modes can set the talker ID
-  // to indicate GPS regardless of actual navigation system used.
-  _navSystem = _talkerID;
-  
-  s = parseTime(s);
-  // ++s;
-  _latitude = parseDegreeMinute(s, 2, &s);
-  if (*s == ',')
-    ++s;
-  else {
-    if (*s == 'S')
-      _latitude *= -1; 
-    s += 2; // Skip N/S and comma
-  }
-  _longitude = parseDegreeMinute(s, 3, &s);
-  if (*s == ',')
-    ++s;
-  else {
-    if (*s == 'W')
-      _longitude *= -1;
-    s += 2; // Skip E/W and comma
-  }
-  _isValid = (*s == '1' || *s == '2');
-  s += 2; // Skip position fix flag and comma
-  _numSat = parseFloat(s, 0, &s);
-  _hdop = parseFloat(s, 1, &s);
-  _altitude = parseFloat(s, 3, &s);
-  _altitudeValid = true;
-  // That's all we care about
-  return true;
+	// If GxGSV messages are received _talker_ID can be changed after
+	// other MicroNMEA sentences. Compatibility modes can set the talker ID
+	// to indicate GPS regardless of actual navigation system used.
+	_navSystem = _talkerID;
+
+	s = parseTime(s);
+	// ++s;
+	_latitude = parseDegreeMinute(s, 2, &s);
+	if (*s == ',')
+		++s;
+	else {
+		if (*s == 'S')
+			_latitude *= -1;
+		s += 2; // Skip N/S and comma
+	}
+	_longitude = parseDegreeMinute(s, 3, &s);
+	if (*s == ',')
+		++s;
+	else {
+		if (*s == 'W')
+			_longitude *= -1;
+		s += 2; // Skip E/W and comma
+	}
+	_isValid = (*s == '1' || *s == '2');
+	s += 2; // Skip position fix flag and comma
+	_numSat = parseFloat(s, 0, &s);
+	_hdop = parseFloat(s, 1, &s);
+	_altitude = parseFloat(s, 3, &s);
+	_altitudeValid = true;
+	// That's all we care about
+	return true;
 }
 
 
 bool MicroNMEA::processRMC(const char* s)
 {
-  // If GxGSV messages are received _talker_ID can be changed after
-  // other MicroNMEA sentences. Compatibility modes can set the talker
-  // ID to indicate GPS regardless of actual navigation system used.
-  _navSystem = _talkerID;
-  
-  s = parseTime(s);
-  _isValid = (*s == 'A');
-  s += 2; // Skip validity and comma
-  _latitude = parseDegreeMinute(s, 2, &s);
-  if (*s == ',')
-    ++s;
-  else {
-    if (*s == 'S')
-      _latitude *= -1; 
-    s += 2; // Skip N/S and comma
-  }
-  _longitude = parseDegreeMinute(s, 3, &s);
-  if (*s == ',')
-    ++s;
-  else {
-    if (*s == 'W')
-      _longitude *= -1;
-    s += 2; // Skip E/W and comma
-  }
-  _speed = parseFloat(s, 3, &s);
-  _course = parseFloat(s, 3, &s);
-  s = parseDate(s);
-  // That's all we care about
-  return true;
-}
+	// If GxGSV messages are received _talker_ID can be changed after
+	// other MicroNMEA sentences. Compatibility modes can set the talker
+	// ID to indicate GPS regardless of actual navigation system used.
+	_navSystem = _talkerID;
 
+	s = parseTime(s);
+	_isValid = (*s == 'A');
+	s += 2; // Skip validity and comma
+	_latitude = parseDegreeMinute(s, 2, &s);
+	if (*s == ',')
+		++s;
+	else {
+		if (*s == 'S')
+			_latitude *= -1;
+		s += 2; // Skip N/S and comma
+	}
+	_longitude = parseDegreeMinute(s, 3, &s);
+	if (*s == ',')
+		++s;
+	else {
+		if (*s == 'W')
+			_longitude *= -1;
+		s += 2; // Skip E/W and comma
+	}
+	_speed = parseFloat(s, 3, &s);
+	_course = parseFloat(s, 3, &s);
+	s = parseDate(s);
+	// That's all we care about
+	return true;
+}
--- src/MicroNMEA.h
+++ src/MicroNMEA.h
@@ -13,176 +13,176 @@
 
 class MicroNMEA {
 public:
-  
-  static const char* skipField(const char* s); 
-  static unsigned int parseUnsignedInt(const char* s, uint8_t len);
-  static long parseFloat(const char* s, uint8_t log10Multiplier,
-			 const char** eptr = NULL);
-  static long parseDegreeMinute(const char* s, uint8_t degWidth,
-				const char** eptr = NULL);
-  static const char* parseToComma(const char* s, char *result = NULL,
-				  int len = 0);
-  static const char* parseField(const char* s, char *result = NULL,
-				int len = 0);
-  static const char* generateChecksum(const char* s, char* checksum);
-  static bool testChecksum(const char* s);
 
-  // Write MicroNMEA sentence to oputput stream. Sentence must start with
-  // '$', the checksum and <CR><NL> terminators will be appended
-  // automatically.
-  static Stream& sendSentence(Stream &s, const char* sentence);
+	static const char* skipField(const char* s);
+	static unsigned int parseUnsignedInt(const char* s, uint8_t len);
+	static long parseFloat(const char* s, uint8_t log10Multiplier,
+						   const char** eptr = NULL);
+	static long parseDegreeMinute(const char* s, uint8_t degWidth,
+								  const char** eptr = NULL);
+	static const char* parseToComma(const char* s, char *result = NULL,
+									int len = 0);
+	static const char* parseField(const char* s, char *result = NULL,
+								  int len = 0);
+	static const char* generateChecksum(const char* s, char* checksum);
+	static bool testChecksum(const char* s);
 
-  // Object with no buffer allocated, must call setBuffer later
-  MicroNMEA(void);
-  
-  // User must decide and allocate the buffer
-  MicroNMEA(void* buffer, uint8_t len);
+	// Write MicroNMEA sentence to oputput stream. Sentence must start with
+	// '$', the checksum and <CR><NL> terminators will be appended
+	// automatically.
+	static Stream& sendSentence(Stream &s, const char* sentence);
 
-  void setBuffer(void* buf, uint8_t len);
-  
-  // Clear all fix information. isValid() will return false, Year,
-  // month and day will all be zero. Hour, minute and second time will
-  // be set to 99. Speed, course and altitude will be set to
-  // LONG_MIN; the altitude validity flag will be false. Latitude and
-  // longitude will be set to 999 degrees.
-  void clear(void);
+	// Object with no buffer allocated, must call setBuffer later
+	MicroNMEA(void);
 
-  // Navigation system, N=GNSS, P=GPS, L=GLONASS, A=Galileo, '\0'=none
-  char getNavSystem(void) const {
-    return _navSystem;
-  }
+	// User must decide and allocate the buffer
+	MicroNMEA(void* buffer, uint8_t len);
 
-  uint8_t getNumSatellites(void) const {
-    return _numSat;
-  }
+	void setBuffer(void* buf, uint8_t len);
 
-  // Horizontal dilution of precision, in tenths
-  uint8_t getHDOP(void) const {
-    return _hdop;
-  }
+	// Clear all fix information. isValid() will return false, Year,
+	// month and day will all be zero. Hour, minute and second time will
+	// be set to 99. Speed, course and altitude will be set to
+	// LONG_MIN; the altitude validity flag will be false. Latitude and
+	// longitude will be set to 999 degrees.
+	void clear(void);
 
-  // Validity of latest fix
-  bool isValid(void) const {
-    return _isValid;
-  }
+	// Navigation system, N=GNSS, P=GPS, L=GLONASS, A=Galileo, '\0'=none
+	char getNavSystem(void) const {
+		return _navSystem;
+	}
 
-  // Latitude in millionths of a degree. North is positive.
-  long getLatitude(void) const {
-    return _latitude;
-  }
-  
-  // Longitude in millionths of a degree. East is positive.
-  long getLongitude(void) const {
-    return _longitude;
-  }
+	uint8_t getNumSatellites(void) const {
+		return _numSat;
+	}
 
-  // Altitude in millimetres.
-  bool getAltitude(long &alt) const {
-    if (_altitudeValid)
-      alt = _altitude;
-    return _altitudeValid;
-  }
-  
-  uint16_t getYear(void) const {
-    return _year;
-  }
-  
-  uint8_t getMonth(void) const {
-    return _month;
-  }
-  
-  uint8_t getDay(void) const {
-    return _day;
-  }
-  
-  uint8_t getHour(void) const {
-    return _hour;
-  }
-  
-  uint8_t getMinute(void) const {
-    return _minute;
-  }
-  
-  uint8_t getSecond(void) const {
-    return _second;
-  }
-  
-  uint8_t getHundredths(void) const {
-    return _hundredths;
-  }
+	// Horizontal dilution of precision, in tenths
+	uint8_t getHDOP(void) const {
+		return _hdop;
+	}
 
-  long getSpeed(void) const {
-    return _speed;
-  }
+	// Validity of latest fix
+	bool isValid(void) const {
+		return _isValid;
+	}
 
-  long getCourse(void) const {
-    return _course;
-  }
+	// Latitude in millionths of a degree. North is positive.
+	long getLatitude(void) const {
+		return _latitude;
+	}
 
-  bool process(char c);
-  
-  void setBadChecksumHandler(void (*handler)(MicroNMEA& nmea)) {
-    _badChecksumHandler = handler;
-  }
-  
-  void setUnknownSentenceHandler(void (*handler)(MicroNMEA& nmea)) {
-    _unknownSentenceHandler = handler;
-  }
+	// Longitude in millionths of a degree. East is positive.
+	long getLongitude(void) const {
+		return _longitude;
+	}
 
-  // Current MicroNMEA sentence. 
-  const char* getSentence(void) const {
-    return _buffer;
-  }
+	// Altitude in millimetres.
+	bool getAltitude(long &alt) const {
+		if (_altitudeValid)
+			alt = _altitude;
+		return _altitudeValid;
+	}
 
-  // Talker ID for current MicroNMEA sentence
-  char getTalkerID(void) const {
-    return _talkerID;
-  }
+	uint16_t getYear(void) const {
+		return _year;
+	}
 
-  // Message ID for current MicroNMEA sentence
-  const char* getMessageID(void) const {
-    return (const char*)_messageID;
-  }
+	uint8_t getMonth(void) const {
+		return _month;
+	}
 
-    
+	uint8_t getDay(void) const {
+		return _day;
+	}
+
+	uint8_t getHour(void) const {
+		return _hour;
+	}
+
+	uint8_t getMinute(void) const {
+		return _minute;
+	}
+
+	uint8_t getSecond(void) const {
+		return _second;
+	}
+
+	uint8_t getHundredths(void) const {
+		return _hundredths;
+	}
+
+	long getSpeed(void) const {
+		return _speed;
+	}
+
+	long getCourse(void) const {
+		return _course;
+	}
+
+	bool process(char c);
+
+	void setBadChecksumHandler(void (*handler)(MicroNMEA& nmea)) {
+		_badChecksumHandler = handler;
+	}
+
+	void setUnknownSentenceHandler(void (*handler)(MicroNMEA& nmea)) {
+		_unknownSentenceHandler = handler;
+	}
+
+	// Current MicroNMEA sentence.
+	const char* getSentence(void) const {
+		return _buffer;
+	}
+
+	// Talker ID for current MicroNMEA sentence
+	char getTalkerID(void) const {
+		return _talkerID;
+	}
+
+	// Message ID for current MicroNMEA sentence
+	const char* getMessageID(void) const {
+		return (const char*)_messageID;
+	}
+
+
 protected:
-  static inline bool isEndOfFields(char c) {
-    return c == '*' || c == '\0' || c == '\r' || c == '\n';
-  }
-  
-  const char* parseTime(const char* s);
-  const char* parseDate(const char* s);
+	static inline bool isEndOfFields(char c) {
+		return c == '*' || c == '\0' || c == '\r' || c == '\n';
+	}
 
-  bool processGGA(const char *s);
-  bool processRMC(const char* s);
-  
+	const char* parseTime(const char* s);
+	const char* parseDate(const char* s);
+
+	bool processGGA(const char *s);
+	bool processRMC(const char* s);
+
 private:
-  // Sentence buffer and associated pointers
-  // static const uint8_t _bufferLen = 83; // 82 + NULL
-  // char _buffer[_bufferLen];
-  uint8_t _bufferLen;
-  char* _buffer;
-  char *_ptr;
+	// Sentence buffer and associated pointers
+	// static const uint8_t _bufferLen = 83; // 82 + NULL
+	// char _buffer[_bufferLen];
+	uint8_t _bufferLen;
+	char* _buffer;
+	char *_ptr;
 
-  // Information from current MicroNMEA sentence
-  char _talkerID;
-  char _messageID[6];
+	// Information from current MicroNMEA sentence
+	char _talkerID;
+	char _messageID[6];
 
-  // Variables parsed and kept for user
-  char _navSystem;
-  bool _isValid;
-  long _latitude, _longitude; // In millionths of a degree
-  long _altitude; // In millimetres
-  bool _altitudeValid;
-  long _speed, _course;
-  uint16_t _year;
-  uint8_t _month, _day, _hour, _minute, _second, _hundredths;
-  uint8_t _numSat;
-  uint8_t _hdop;
+	// Variables parsed and kept for user
+	char _navSystem;
+	bool _isValid;
+	long _latitude, _longitude; // In millionths of a degree
+	long _altitude; // In millimetres
+	bool _altitudeValid;
+	long _speed, _course;
+	uint16_t _year;
+	uint8_t _month, _day, _hour, _minute, _second, _hundredths;
+	uint8_t _numSat;
+	uint8_t _hdop;
 
-  void (*_badChecksumHandler)(MicroNMEA &nmea);
-  void (*_unknownSentenceHandler)(MicroNMEA &nmea);
-  
+	void (*_badChecksumHandler)(MicroNMEA &nmea);
+	void (*_unknownSentenceHandler)(MicroNMEA &nmea);
+
 };
 
 
