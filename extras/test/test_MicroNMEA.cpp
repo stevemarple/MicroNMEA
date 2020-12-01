7dad3015a145c1055444ccdd31136f558b2854a0
Steve Marple <s.marple@systemloco.com>
2020-12-01 12:29:33 +0000

    Regression test program fixes
    Do not pass comments to the MicroNMEA::process()
    
    After each sentence has been processed call clear() so that all state
    information is reset.
    
    Date is not present in GGA commands. Print just time in result.
    
    Fix regression test output as a result of these changes.

--- extras/test/test_MicroNMEA.cpp
+++ extras/test/test_MicroNMEA.cpp
@@ -15,18 +15,18 @@ void unknownSentenceHandler(MicroNMEA &nmea)
 void badChecksumHandler(MicroNMEA &nmea)
 {
   // Don't print messages for empty lines or comments
-  const char *s = nmea.getSentence(); 
+  const char *s = nmea.getSentence();
   if (s && strlen(s) && s[0] == '$' ) {
     char checksum[3];
     MicroNMEA::generateChecksum(s, checksum);
     checksum[2] = '\0';
     cout << "Bad checksum for \"" << nmea.getSentence() << '"' << endl
-	 << "Checksum should be " << checksum << endl
-	 << separator << endl;
+         << "Checksum should be " << checksum << endl
+         << separator << endl;
   }
 }
 
-ostream& formatDateTime(ostream& s, MicroNMEA& nmea)
+ostream& formatDateTime(ostream& s, const MicroNMEA& nmea)
 {
   s << setfill('0')
     << setw(4) << nmea.getYear() << '-'
@@ -39,48 +39,75 @@ ostream& formatDateTime(ostream& s, MicroNMEA& nmea)
   return s;
 }
 
+ostream& formatTime(ostream& s, const MicroNMEA& nmea)
+{
+  s << setfill('0')
+    << setw(2) << int(nmea.getHour()) << ':'
+    << setw(2) << int(nmea.getMinute()) << ':'
+    << setw(2) << int(nmea.getSecond()) << '.'
+    << setw(2) << int(nmea.getHundredths());
+  return s;
+}
+
 int main(void)
 {
   char buffer[85];
   MicroNMEA nmea(buffer, sizeof(buffer));
-  
+
   nmea.setBadChecksumHandler(badChecksumHandler);
   nmea.setUnknownSentenceHandler(unknownSentenceHandler);
-  
+
+  bool isComment = false;
+  bool isNewline = true;
+
   while (!cin.eof()) {
     char c;
     cin.get(c);
+    if (isNewline && c == '#')
+      isComment = true;
+
+    isNewline = (c == '\r' || c == '\n');
+    if (isNewline)
+      isComment = false;
+
     cout.put(c);
     cout.flush();
-    if (nmea.process(c)) {
+    if (!isComment && nmea.process(c)) {
       if (nmea.isValid()) {
-	cout << "Sentence    " << nmea.getSentence() << endl
-	     << "Valid        " << (nmea.isValid() ? "true" : "false") << endl
-	     << "TalkerID     " << nmea.getTalkerID() << endl
-	     << "MessageID    " << nmea.getMessageID() << endl;
+        cout << "Sentence    " << nmea.getSentence() << endl
+             << "Valid        " << (nmea.isValid() ? "true" : "false") << endl
+             << "TalkerID     " << nmea.getTalkerID() << endl
+             << "MessageID    " << nmea.getMessageID() << endl;
 
-	cout << "Date/time    ";
-	formatDateTime(cout, nmea);
-	cout << endl;
-	
-	cout << fixed << setprecision(6)
-	     << "Latitude     " << nmea.getLatitude()
-	     << " (" << (nmea.getLatitude() / 1e6) << " deg)" << endl
-	     << "Longitude    " << nmea.getLongitude()
-	     << " (" << (nmea.getLongitude() / 1e6) << " deg)" << endl;
+        if (strcmp(nmea.getMessageID(), "GGA") == 0) {
+          cout << "Time         ";
+          formatTime(cout, nmea);
+          cout << endl;
+        } else {
+          cout << "Date/time    ";
+          formatDateTime(cout, nmea);
+          cout << endl;
+        }
 
-	long altitude;
-	if (nmea.getAltitude(altitude))
-	  cout << fixed << setprecision(3)
-	       << "Alt. valid   true" << endl
-	       << "Altitude     " << altitude << " ("
-	       << fixed << setprecision(3) << (altitude/1e3) << "m)" << endl;
-	else
-	  cout << "Alt. valid   false" << endl;
+        cout << fixed << setprecision(6)
+             << "Latitude     " << nmea.getLatitude()
+             << " (" << (nmea.getLatitude() / 1e6) << " deg)" << endl
+             << "Longitude    " << nmea.getLongitude()
+             << " (" << (nmea.getLongitude() / 1e6) << " deg)" << endl;
+
+        long altitude;
+        if (nmea.getAltitude(altitude))
+          cout << fixed << setprecision(3)
+               << "Alt. valid   true" << endl
+               << "Altitude     " << altitude << " ("
+               << fixed << setprecision(3) << (altitude / 1e3) << "m)" << endl;
+        else
+          cout << "Alt. valid   false" << endl;
 
 
-	cout << separator << endl;
+        cout << separator << endl;
       }
+      nmea.clear();
     }
   }
 
